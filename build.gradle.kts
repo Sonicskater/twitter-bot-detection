37808ae8e49ab40efd02e88bb374a79838358f06
Jacob Stanich <jacob.stanich1@ucalgary.ca>
2021-11-07 16:28:23 -0700

    Setting up project

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,10 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+tasks.withType<JavaExec>(){
+    jvmArgs = listOf("-Xms8g","-Xmx8g")
+}
+
 application {
     mainClass.set("MainKt")
 }
