fb1787ea3b2129a56aa48c162e179ce0681cb957
Jacob Stanich <jacob.stanich1@ucalgary.ca>
2021-11-10 22:13:08 -0700

    Added HighFollowingToFollowersRation feature

---
+++ src/main/java/HighFollowingToFollowersRatio.java
@@ -0,0 +1,12 @@
+import org.jetbrains.annotations.NotNull;
+import java.util.Objects;
+
+public class HighFollowingToFollowersRatio implements BinaryFeature {
+
+    @Override
+    public boolean hasFeature(@NotNull User user) {
+        double following = Objects.requireNonNull(user.getProfile()).getFollowing();
+        double followers = Objects.requireNonNull(user.getProfile()).getFollowers();
+        return (following / followers) > 0.7;
+    }
+}
