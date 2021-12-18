e48a9104013ae6c75c539d4ce8bf154b5038b049
Devon Hockley <devon@hockley.ca>
2021-12-18 15:21:18 -0700

    Reduced language count

--- src/main/kotlin/features/jis/IsMultilingual.kt
+++ src/main/kotlin/features/jis/IsMultilingual.kt
@@ -1,12 +1,13 @@
 package features.jis
 
+import com.github.pemistahl.lingua.api.Language
 import com.github.pemistahl.lingua.api.LanguageDetectorBuilder
 import features.BinaryFeature
 import features.User
 
 class IsMultilingual : BinaryFeature {
 
-    val detector = LanguageDetectorBuilder.fromAllLanguagesWithLatinScript().build()
+    val detector = LanguageDetectorBuilder.fromLanguages(Language.ENGLISH, Language.HINDI, Language.RUSSIAN, Language.SPANISH, Language.FRENCH, Language.GERMAN).build()
 
     override fun hasFeature(user: User): Boolean {
         val langs = user.tweets?.map {
