1cc3b69b057463796754044565abc21d12095430
Devon Hockley <devon@hockley.ca>
2021-12-18 12:12:54 -0700

    added SMILE lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1-fixed_streams")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.apache.commons:commons-math3:3.6.1")
+
+    implementation("com.github.haifengl:smile-kotlin:2.6.0")
 }
 
 
